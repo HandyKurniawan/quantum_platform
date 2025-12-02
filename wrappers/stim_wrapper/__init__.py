ef37e4595dcd10dbb11d6dcdd165cb30749a45af
Handy Kurniawan <handykhandy@gmail.com>
2025-12-02 10:31:34 +0100

    update code

--- wrappers/stim_wrapper/__init__.py
+++ wrappers/stim_wrapper/__init__.py
@@ -1,4 +1,4 @@
-from .stim_wrapper import (simulate_stim_polar_code, generate_circuit_extraction_syndrome_stim, simulate_batch_and_save_result_polar,
+from .stim_wrapper import (simulate_stim_polar_code, generate_circuit_extraction_syndrome, generate_circuit_extraction_syndrome_stim, simulate_batch_and_save_result_polar,
                            calculate_logical_error_result_polar, 
                            convert_i_to_meas_type, noisy_cx, noisy_h, noisy_x, noisy_z, noisy_reset, noisy_measurement,
                            generate_qiskit_polar_code, compiled_to_qiskit_hardware, get_backend_information,
@@ -20,6 +20,7 @@ from .stim_wrapper import (simulate_stim_polar_code, generate_circuit_extraction
 
 __all__ = [
     "simulate_stim_polar_code",
+    "generate_circuit_extraction_syndrome",
     "generate_circuit_extraction_syndrome_stim",
     "simulate_batch_and_save_result_polar",
     "calculate_logical_error_result_polar",
