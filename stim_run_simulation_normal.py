265c267871e6500433fe3b25a270fd76e0c92c8c
Handy Kurniawan <handykhandy@gmail.com>
2025-12-22 13:45:52 +0100

    update result

--- stim_run_simulation_normal.py
+++ stim_run_simulation_normal.py
@@ -125,8 +125,8 @@ if __name__ == "__main__":
     pass
 
     lstate_values = ["x"]
-    sim_type_values = ["normal"]
-    # sim_type_values = ["normal", "m1"]
+    # sim_type_values = ["normal"]
+    sim_type_values = ["normal", "m1"]
     n_values = [4]
     # p_error_values = [0.01, 0.005]
     # # i_values = [4]
@@ -136,8 +136,11 @@ if __name__ == "__main__":
     # p_error_values = [0.01, 0.005, 0.001, 0.0005, 0.0001]
     p_error_values = [0.001]
     # i_values = range(2, (2**n_values[0]))
-    i_values = [2]
+    # i_values = [2]
     shots_values = [int(1e7)]
+
+    lstate_values = ["z"]
+    i_values = [8, 12, 14, 15]
     
     for _ in range(10000):
         run_all(lstate_values, sim_type_values, n_values, p_error_values, i_values, shots_values)
