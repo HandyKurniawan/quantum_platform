382fbb19f054d689326927280e017ed0cabcfcfb
Handy Kurniawan <handykhandy@gmail.com>
2026-03-30 13:31:11 +0200

    update and fix SCL

--- test_encoder_val_decoder_anqi.py
+++ test_encoder_val_decoder_anqi.py
@@ -62,20 +62,31 @@ def call_experiment(n, lstate, sim_type, i, p_error, shots, seed, K, L, p_zpos =
 
     # 2. Create a mask of ALL Trues (all frozen)
     frozen_bits_mask = np.ones(N, dtype=np.int32)
+    gauge_bits_mask = np.zeros(N, dtype=np.int32)
 
     # 3. Punch a hole EXACTLY at zpos for the info bit
     frozen_bits_mask[zpos] = 0
-    # frozen_bits_mask[zpos:] = 0
+
+    if lstate.upper() == "Z":
+        
+        gauge_bits_mask[zpos+1:] = 1
+    elif lstate.upper() == "X":
+        gauge_bits_mask[:zpos] = 1
+    
+    # print(frozen_bits_mask)
+    # print(gauge_bits_mask)
+    
     frozen_bits_mask = list(frozen_bits_mask)
+    gauge_bits_mask = list(gauge_bits_mask)
     # frozen_bits_mask[N-1-zpos] = 1
 
-    print(frozen_bits_mask)
+    
 
     results = simulate_stim_polar_code_normal(n, lstate, sim_type, i, p_error, shots, seed)
     # accepted_states, data_qubit_states = get_q1prep_accepted_states(n, lstate, results, zpos_list)
 
     # Initialize the decoder
-    decoder = PyDecoderPolarSCL(K, N, L, frozen_bits_mask)
+    decoder = PyDecoderPolarSCL(K, N, L, frozen_bits_mask, gauge_bits_mask)
 
     count_accept, count_logerror, count_undecided, ler, detection_time, decoding_time = get_logical_error_on_accepted_states(n, lstate.upper(), results, zpos_list)
     count_accept_m1, count_logerror_m1, count_undecided_m1, ler_m1, detection_time_m1, decoding_time_m1 = get_logical_error_on_accepted_states_SCL(n, lstate.upper(), results, decoder, p_error, zpos_list)
@@ -84,14 +95,14 @@ def call_experiment(n, lstate, sim_type, i, p_error, shots, seed, K, L, p_zpos =
     print(count_accept_m1, count_logerror_m1, count_undecided_m1, 1-ler_m1, detection_time_m1, decoding_time_m1)
 
 n = 4
-lstate = 'x'
-i = 5
+lstate = 'z'
+# i = 2
 # i = 7
-# i = 15
-p_error = 0.02
-shots = 100000
-# seed = 10000
-seed = random.randint(1, 99999999)
+i = 12
+p_error = 0.05
+shots = 20000
+seed = 10000
+# seed = random.randint(1, 99999999)
 K = 1
 L = 1
 
